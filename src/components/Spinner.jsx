8d3dae576c9468e00d3c1bef5421b91a4089721c
Alex7vzla <alejandro117925@gmail.com>
2022-08-07 13:22:27 -0400

    Details

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className='bg'>
+        <div className="loadingio-spinner-dual-ring-pfuj939dx59">
+            <div className="ldio-ty22qldf9yk">
+                <div>
+                    </div>
+                        <div>
+                            <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>   
+    </div>
+  )
+}
+
+export default Spinner
